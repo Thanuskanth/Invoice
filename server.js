f6b744421f94b4b914ae5fba7e8717d777bb5ba7
Thanuskanth <thanus1207.tt@gmail.com>
2020-11-27 10:38:07 +0530

    vertion 1.0.0

--- server.js
+++ server.js
@@ -46,9 +46,9 @@ app.use('/debit', debitRouter);
 app.use('/debit_description', debitDesRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('react-material-dashboard-master/build'));
+    app.use(express.static(' client/build'));
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'react-material-dashboard-master', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 // mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log("database connected "))
