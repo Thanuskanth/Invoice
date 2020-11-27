64e13abb1d8468ad5d5c51e8d76501653442c2a4
Thanuskanth <thanus1207.tt@gmail.com>
2020-11-27 10:50:43 +0530

    vertion 1.0.0

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use('/debit_description', debitDesRouter);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(' client/build'));
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
     });
 }
 // mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log("database connected "))
