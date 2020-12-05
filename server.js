3073bca65a61414e6b47f7fd644fede1c143a45d
Thanuskanth <thanus1207.tt@gmail.com>
2020-12-05 21:51:37 +0530

    Your awesome message

--- server.js
+++ server.js
@@ -53,12 +53,12 @@ app.use('/debit', debitRouter);
 app.use('/service', serviceRouter);
 app.use('/debit_description', debitDesRouter);
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     app.use(express.static(' client/build'));
     app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
     });
-}
+// }
 // mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log("database connected "))
 
 app.listen(port, () => console.log(`server connect port ${port}`))
